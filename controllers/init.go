16c84730e6133db1d40cf47bc97969a27043168d
Henry Hirsch <henry@w3-net.de>
2024-06-19 08:37:52 +0200

    feat: implement init command

--- controllers/init.go
+++ controllers/init.go
@@ -1,10 +1,27 @@
 package controllers
 
+import (
+	"fmt"
+	"os"
+)
+
 func isInitialized() bool {
+	dirName := ".builder"
+
+	if _, err := os.Stat(dirName); os.IsNotExist(err) {
+		return false
+	}
 	return true
 }
 
-func createDirectories() {}
+func createDirectories() {
+	dirName := ".builder"
+	err := os.Mkdir(dirName, 0755)
+	if err != nil {
+		fmt.Println("Error creating directory:", err)
+		return
+	}
+}
 
 func Init() string {
 	if isInitialized() {
