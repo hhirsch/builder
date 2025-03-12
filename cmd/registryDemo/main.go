50d22d8f0ff0c98692e35d3279a247e247020b58
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-03-12 15:57:19 +0100

    fix: error checking on registry demo

--- cmd/registryDemo/main.go
+++ cmd/registryDemo/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	filePath := "/tmp/demo.reg"
 	registry := models.NewRegistry(filePath)
-	registry.Load()
+	error := registry.Load()
+	if error != nil {
+		fmt.Println("Error loading registry:", error)
+		return
+	}
 	registry.Register("Registry.BEE.Bar", "dataxx")
 	registry.Register("Registry.BEE.Baz", "more dataxx")
-	error := registry.Save()
+	error = registry.Save()
 	if error != nil {
 		fmt.Println("Error saving data:", error)
 		return
