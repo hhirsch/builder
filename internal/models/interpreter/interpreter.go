7a6f157077dce9d0f14d896f037eda1a4e4af16c
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-03-13 10:32:24 +0100

    fix: clean up code smells in interpreter

--- internal/models/interpreter/interpreter.go
+++ internal/models/interpreter/interpreter.go
@@ -14,7 +14,6 @@ type Interpreter struct {
 	logger              *helpers.Logger
 	environment         *models.Environment
 	registry            *models.Registry
-	step                string
 	commands            map[string]com.Command
 	onlineCommands      map[string]com.Command
 	variables           map[string]string
@@ -24,7 +23,10 @@ type Interpreter struct {
 func NewInterpreter(environment *models.Environment) *Interpreter {
 	logger := environment.GetLogger()
 	registry := models.NewRegistry(environment.GetGlobalRegistryPath())
-	registry.Load()
+	err := registry.Load()
+	if err != nil {
+		logger.Fatalf("Unable to load registry: %s", err.Error())
+	}
 	variables := map[string]string{}
 	commands := map[string]com.Command{
 		"step":      com.NewStepCommand(environment),
