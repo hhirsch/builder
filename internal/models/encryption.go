f0b758b5137404914b8ffeaba2acf5b670861155
Henry Hirsch <2451426+hhirsch@users.noreply.github.com>
2025-04-01 18:20:06 +0200

    feat: proper error messages

--- internal/models/encryption.go
+++ internal/models/encryption.go
@@ -26,7 +26,7 @@ func NewEncryption(environment *Environment) (*Encryption, error) {
 
 	block, _ := pem.Decode(key)
 	if block == nil {
-		return nil, fmt.Errorf("Failed to parse PEM block containing the private key.")
+		return nil, fmt.Errorf("failed to parse PEM block containing the private key")
 	}
 
 	var rsaPrivateKey *rsa.PrivateKey
@@ -35,21 +35,21 @@ func NewEncryption(environment *Environment) (*Encryption, error) {
 		logger.Info("Private key is in PKCS#1 format.")
 		rsaPrivateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to parse PEM block containing the private PKCS#1 key: %v", err)
+			return nil, fmt.Errorf("#[file:line] failed to parse PEM block containing the private PKCS#1 key: %w", err)
 		}
 	case "PRIVATE KEY":
 		logger.Info("The key is in PKCS#8 format.")
 		rsaPrivateKeyBinary, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to parse PEM block containing the private PKCS#8 key: %v", err)
+			return nil, fmt.Errorf("#[file:line] failed to parse PEM block containing the private PKCS#8 key: %w", err)
 		}
 		var ok bool
 		rsaPrivateKey, ok = rsaPrivateKeyBinary.(*rsa.PrivateKey)
 		if !ok {
-			return nil, fmt.Errorf("Not an RSA private key in file %s.", environment.GetKeyPath())
+			return nil, fmt.Errorf("#[file:line] not an RSA private key in file %s", environment.GetKeyPath())
 		}
 	default:
-		return nil, fmt.Errorf("Unknown key format.")
+		return nil, fmt.Errorf("unknown key format")
 	}
 
 	return &Encryption{environment: environment, logger: logger, privateKey: rsaPrivateKey}, nil
@@ -58,17 +58,17 @@ func NewEncryption(environment *Environment) (*Encryption, error) {
 func NewEncryptionPkcs1(environment *Environment) *Encryption {
 	key, error := os.ReadFile(environment.GetKeyPath())
 	if error != nil {
-		environment.GetLogger().Fatalf("Error reading file: %v", error)
+		environment.GetLogger().Fatalf("error reading file: %v", error)
 	}
 
 	block, _ := pem.Decode(key)
 	if block == nil || block.Type != "RSA PRIVATE KEY" {
-		environment.GetLogger().Fatal("Failed to parse PEM block containing the private key.")
+		environment.GetLogger().Fatal("failed to parse PEM block containing the private key.")
 	}
 
 	rsaPrivateKey, error := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if error != nil {
-		environment.GetLogger().Fatalf("Failed to parse PKCS#1 private key: %v", error)
+		environment.GetLogger().Fatalf("failed to parse PKCS#1 private key: %v", error)
 	}
 
 	return &Encryption{environment: environment, logger: environment.GetLogger(), privateKey: rsaPrivateKey}
@@ -77,22 +77,22 @@ func NewEncryptionPkcs1(environment *Environment) *Encryption {
 func NewEncryptionPkcs8(environment *Environment) *Encryption {
 	key, error := os.ReadFile(environment.GetKeyPath())
 	if error != nil {
-		environment.GetLogger().Fatalf("Error reading file: %v", error)
+		environment.GetLogger().Fatalf("error reading file: %v", error)
 	}
 
 	block, _ := pem.Decode(key)
 	if block == nil || block.Type != "PRIVATE KEY" {
-		environment.GetLogger().Fatal("Failed to parse PEM block containing the private key.")
+		environment.GetLogger().Fatal("failed to parse PEM block containing the private key.")
 	}
 
 	priv, error := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if error != nil {
-		environment.GetLogger().Fatalf("Failed to parse PKCS#8 private key: %v", error)
+		environment.GetLogger().Fatalf("failed to parse PKCS#8 private key: %v", error)
 	}
 
 	rsaPrivKey, ok := priv.(*rsa.PrivateKey)
 	if !ok {
-		environment.GetLogger().Fatalf("Not an RSA private key in file %s.", environment.GetKeyPath())
+		environment.GetLogger().Fatalf("not an RSA private key in file %s.", environment.GetKeyPath())
 	}
 
 	return &Encryption{environment: environment, logger: environment.GetLogger(), privateKey: rsaPrivKey}
@@ -103,7 +103,7 @@ func (this *Encryption) Encrypt(plainValue string) (encryptedValue string, err e
 	var encryptedValueByteSlices []byte
 	encryptedValueByteSlices, err = rsa.EncryptOAEP(sha256.New(), rand.Reader, &publicKey, []byte(plainValue), nil)
 	if err != nil {
-		this.logger.Fatalf("Encryption failed: %v", err)
+		this.logger.Errorf("Encryption failed: %v", err)
 		return "", err
 	}
 	return string(encryptedValueByteSlices), nil
